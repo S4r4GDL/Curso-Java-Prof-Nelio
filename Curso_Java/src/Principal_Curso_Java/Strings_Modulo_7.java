63ab85ce704c8332ab5831a34473b417bf9fc9d1
sarag <saragdl@gmail.com>
2024-02-08 21:57:51 -0300

    Create Stringd_Modulo_7

---
+++ Curso_Java/src/Principal_Curso_Java/Strings_Modulo_7.java
@@ -0,0 +1,29 @@
+package Principal_Curso_Java;
+
+import java.util.Scanner;
+
+public class Strings_Modulo_7 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Text ex ( BIRD-DOG-CAT-RACCOON-FISH || hello-world:\n");
+		String text1 = input.nextLine();
+		
+		String[] text2 = text1.split("-");
+		
+		System.out.println("First index: " + text2[0]);
+		
+		String text3 = " ABC DEF ghi jkl ";
+		
+		System.out.println("toUpperCase: " + text3.toUpperCase());
+		System.out.println("toLowerCase: " + text3.toLowerCase());
+		System.out.println("trim: " + "-" + text3.trim() + "-");
+		
+		input.close();
+		
+	
+		
+	}
+
+}
