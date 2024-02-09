2a63688ed24a976a7dc1b41097c1a8f8e528840f
sarag <saragdl@gmail.com>
2024-02-08 23:51:12 -0300

    Create Product and Calculo_Produtos_Modulo_8

---
+++ Curso_Java/src/principal_curso_java_md8_ao_md16/Calculo_Produtos_Modulo_8.java
@@ -0,0 +1,36 @@
+package principal_curso_java_md8_ao_md16;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Calculo_Produtos_Modulo_8 {
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner input = new Scanner(System.in);
+		
+		Product prod = new Product();
+		
+		System.out.println("Enter product data:");
+		System.out.print("Name: ");
+		prod.setName(input.nextLine());
+		System.out.print("Price: ");
+		prod.setPrice(input.nextDouble());
+		System.out.print("Quantity in stock: ");
+		prod.setQuantity(input.nextInt());
+		
+		System.out.println("Product data: " + prod);
+		
+		System.out.print("Enter the number of products to be added in stock: ");
+		prod.addProducts(input.nextInt());
+		System.out.println("Updated data: " + prod);
+		
+		System.out.print("Enter the number of products to be removed from stock: ");
+		prod.removeProducts(input.nextInt());
+		System.out.println("Updated data: " + prod);
+		
+		
+		input.close();
+	}
+
+}
---
+++ Curso_Java/src/principal_curso_java_md8_ao_md16/Product.java
@@ -0,0 +1,51 @@
+package principal_curso_java_md8_ao_md16;
+
+public class Product {
+	String name;
+	double price;
+	int quantity;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+	
+	public double totalValueInStock() {
+		return price * quantity;
+	}
+	
+	public void addProducts(int quantity) {
+		this.quantity += quantity;
+	}
+	
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity;
+	}
+	
+	public String toString() {
+		return name 
+				+ ", $"
+				+ String.format("%.2f", price)
+				+  " units, Total: $ "
+				+ String.format("%.2f", this.totalValueInStock());
+	}
+}
