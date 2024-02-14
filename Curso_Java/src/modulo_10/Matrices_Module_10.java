48a92d4f8296b11ee21121d73c4fc38a8a900fff
sarag <saragdl@gmail.com>
2024-02-14 14:46:23 -0300

    Create Matrices_Module_10

---
+++ Curso_Java/src/modulo_10/Matrices_Module_10.java
@@ -0,0 +1,12 @@
+package modulo_10;
+
+import java.util.Scanner;
+
+public class Matrices_Module_10 {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+	}
+	
+}
