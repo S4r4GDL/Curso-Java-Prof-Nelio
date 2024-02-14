0216347b4d3b6f1dd0a1fd776438d18930c14b87
sarag <saragdl@gmail.com>
2024-02-14 12:22:22 -0300

    Create ForEach_Module_10

---
+++ Curso_Java/src/modulo_10/ForEach_Module_10.java
@@ -0,0 +1,12 @@
+package modulo_10;
+
+public class ForEach_Module_10 {
+	public static void main(String[] args) {
+		
+		String[] Sentences = new String[] {"Sara", "Adelane", "Maria"};
+		
+		for (String sentence : Sentences) {
+			System.out.println(sentence);
+		}
+	}
+}
