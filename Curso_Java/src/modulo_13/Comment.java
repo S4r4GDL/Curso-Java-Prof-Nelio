12e8ed8a6a80a483fca49cbdfd6299a1bad89a43
sarag <saragdl@gmail.com>
2024-02-25 13:41:18 -0300

    Create Comment

---
+++ Curso_Java/src/modulo_13/Comment.java
@@ -0,0 +1,14 @@
+package modulo_13;
+
+public class Comment {
+	private String text;
+
+	public Comment(String text) {
+		this.text = text;
+	}
+	
+	public String getText() {
+		return text;
+	}
+	
+}
