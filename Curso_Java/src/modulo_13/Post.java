52d0777b97cd30286bc7240d9dc1cb3a8141e458
sarag <saragdl@gmail.com>
2024-02-25 14:05:49 -0300

    Update Post

--- Curso_Java/src/modulo_13/Post.java
+++ Curso_Java/src/modulo_13/Post.java
@@ -1,21 +1,25 @@
 package modulo_13;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Post {
 	private String title;
 	private String content;
 	private Date moment;
 	private Integer likes;
+	private List<Comment> commentsList; 
 	
 	public Post() {
-		
+		commentsList = new ArrayList<Comment>();
 	}
 	public Post(String title, String content, Date moment, Integer likes) {
 		this.title = title;
 		this.content = content;
 		this.moment = moment;
 		this.likes = likes;
+		commentsList = new ArrayList<Comment>();
 	}
 	
 	public String getTitle() {
@@ -45,7 +49,14 @@ public class Post {
 	public void removeLikes(Integer likes) {
 		this.likes -= likes;
 	}
-	
-	
+	public List<Comment> getCommentsList() {
+		return commentsList;
+	}
+	public void addComment(Comment comment) {
+		commentsList.add(comment);
+	}
+	public void removeComment(Comment comment) {
+		commentsList.remove(comment);
+	}
 	
 }
